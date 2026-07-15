8d83f1f55dc1c549cec899a519248bbe923a510a
Yasmine <yasmineahmedali1@gmail.com>
2026-07-15 20:54:50 +0300

    Stage 4: full CRUD

--- Week two/task-api/controller/task.controller.js
+++ Week two/task-api/controller/task.controller.js
@@ -24,7 +24,7 @@ export const getTaskByIdController = (req, res) => {
 };
 
 export const createTaskController = (req, res) => {
-    const Id = tasks[tasks.length - 1].id+ 1;
+  const Id = tasks[tasks.length - 1].id + 1;
   if (!req.body.title) {
     return res.status(400).json({
       error: "The title of the task is missing",
@@ -40,4 +40,70 @@ export const createTaskController = (req, res) => {
     message: "the task created successfully",
     data: newTask,
   });
+};
+
+export const updateTaskController = (req, res) => {
+  const Id = Number(req.params.id);
+  const result = tasks.find((el) => {
+    return el.id === Id;
+  });
+  if (!result) {
+    return res.status(404).json({
+      error: `Task ${Id} not found`,
+    });
+  }
+  if (
+    typeof req.body.title === "undefined" &&
+    typeof req.body.done === "undefined"
+  ) {
+    return res.status(400).json({
+      error: "empty/invalid data",
+    });
+  }
+  if (
+    typeof req.body.title !== "undefined" &&
+    (typeof req.body.title !== "string" || req.body.title.trim() === "")
+  ) {
+    return res.status(400).json({
+      error: "Title must be a non-empty string",
+    });
+  }
+  if (
+    typeof req.body.done !== "undefined" &&
+    typeof req.body.done !== "boolean"
+  ) {
+    return res.status(400).json({
+      error: "Done must be a boolean",
+    });
+  }
+  const updatedTask = {
+    id: Id,
+    title: req.body.title ?? result.title,
+    done: req.body.done ?? result.done,
+  };
+  const idx = tasks.findIndex((el) => {
+    return el.id === Id;
+  });
+  tasks[idx] = updatedTask;
+  res.status(200).json({
+    message: "The task is updated successfully",
+    data: updatedTask,
+  });
+};
+
+export const deleteTaskController = (req, res) => {
+  const Id = Number(req.params.id);
+  const result = tasks.find((el) => {
+    return el.id === Id;
+  });
+  if (!result) {
+    return res.status(404).json({
+      error: `unkown id ${Id}`,
+    });
+  }
+  const idx = tasks.findIndex((el) => {
+    return el.id === Id;
+  });
+  tasks.splice(idx, 1);
+  res.status(204).send();
 };
--- Week two/task-api/routes/task.route.js
+++ Week two/task-api/routes/task.route.js
@@ -3,12 +3,16 @@ import {
   getAllTaskController,
   getTaskByIdController,
   createTaskController,
+  updateTaskController,
+  deleteTaskController,
 } from "../controller/task.controller.js";
 
 const router = Router();
 
 router.get("/", getAllTaskController);
 router.get("/:id", getTaskByIdController);
-router.post('/', createTaskController);
+router.post("/", createTaskController);
+router.put('/:id', updateTaskController);
+router.delete('/:id', deleteTaskController);
 
 export default router;
