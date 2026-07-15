16169792138a1f8ca0d99183cf1e6d66e1116240
Yasmine <yasmineahmedali1@gmail.com>
2026-07-15 18:45:42 +0300

    Stage 3: create with validation

--- Week two/task-api/controller/task.controller.js
+++ Week two/task-api/controller/task.controller.js
@@ -16,10 +16,28 @@ export const getTaskByIdController = (req, res) => {
     return res.status(404).json({
       error: `Task ${Id} not found`,
     });
-      
   }
   res.status(200).json({
     message: `The task ${Id} is fetched successfully`,
     data: result,
+  });
+};
+
+export const createTaskController = (req, res) => {
+    const Id = tasks[tasks.length - 1].id+ 1;
+  if (!req.body.title) {
+    return res.status(400).json({
+      error: "The title of the task is missing",
+    });
+  }
+  const newTask = {
+    id: Id,
+    title: req.body.title,
+    done: false,
+  };
+  tasks.push(newTask);
+  res.status(201).json({
+    message: "the task created successfully",
+    data: newTask,
   });
 };
--- Week two/task-api/routes/task.route.js
+++ Week two/task-api/routes/task.route.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
-import { getAllTaskController,getTaskByIdController } from "../controller/task.controller.js";
-
+import {
+  getAllTaskController,
+  getTaskByIdController,
+  createTaskController,
+} from "../controller/task.controller.js";
 
 const router = Router();
 
-
-
-router.get('/',getAllTaskController);
-router.get('/:id', getTaskByIdController);
-
+router.get("/", getAllTaskController);
+router.get("/:id", getTaskByIdController);
+router.post('/', createTaskController);
 
 export default router;
