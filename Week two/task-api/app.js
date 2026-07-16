dee3541fd50cf1eec52846deb195d79d9aba2dc2
Yasmine <yasmineahmedali1@gmail.com>
2026-07-16 12:43:24 +0300

    Stage 5: Swagger UI

--- Week two/task-api/app.js
+++ Week two/task-api/app.js
@@ -1,11 +1,18 @@
 import express from "express";
 import router from "./routes/task.route.js";
-
+import swaggerUi from "swagger-ui-express";
+import openapiDocument from "./openapi.json" with { type: "json" };
 
 const app = express();
 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+    "/docs",
+    swaggerUi.serve,
+    swaggerUi.setup(openapiDocument)
+);
 
 app.get("/", (req, res) => {
   res.status(200).json({
@@ -21,6 +28,6 @@ app.get("/health", (req, res) => {
   });
 });
 
-app.use('/tasks', router);
+app.use("/tasks", router);
 
 export default app;
