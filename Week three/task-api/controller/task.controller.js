a7869c56738c9fc57bb35f826465c180972e72cc
Yasmine <yasmineahmedali1@gmail.com>
2026-07-26 21:35:52 +0300

    Stage 1: database read endpoints

--- Week three/task-api/controller/task.controller.js
+++ Week three/task-api/controller/task.controller.js
@@ -1,28 +1,41 @@
 import { tasks } from "../Data/tasks.js";
+import db from "../database/database.js";
 
 export const getAllTaskController = (req, res) => {
   const query = req.query.done;
   const searchQuery = req.query.search;
-  if (typeof query !== 'undefined' && query !== 'true' && query !== 'false') {
+  const done = query === "true" ? 1 : 0;
+  if (typeof query !== "undefined" && query !== "true" && query !== "false") {
     return res.status(400).json({
-      error:'Enter valid query '
-    })
+      error: "Enter valid query ",
+    });
   }
   let ans;
-  if (typeof query !== 'undefined'&& typeof searchQuery !=='undefined' ) {
-    ans = tasks.filter((el) => {
-      return String(el.done) === query && el.title.includes(searchQuery);
-    })
-  }else  if (typeof query !== 'undefined' ) {
-    ans = tasks.filter((el) => {
-      return String(el.done) === query;
-    })
-  } else  if (typeof searchQuery !== 'undefined' ) {
-    ans = tasks.filter((el) => {
-      return el.title.includes(searchQuery);
-    })
+  if (typeof query !== "undefined" && typeof searchQuery !== "undefined") {
+    ans = db
+      .prepare(
+        `SELECT * FROM tasks
+                      WHERE done = ?
+                      AND title LIKE ?`,
+      )
+      .all(done, `%${searchQuery}%`);
+  } else if (typeof query !== "undefined") {
+    ans = db
+      .prepare(
+        `SELECT * FROM tasks
+                      WHERE done = ?
+                     `,
+      )
+      .all(done);
+  } else if (typeof searchQuery !== "undefined") {
+    ans = db
+      .prepare(
+        `SELECT * FROM tasks
+                      WHERE title LIKE ?`,
+      )
+      .all(`%${searchQuery}%`);
   } else {
-    ans = tasks;
+    ans = db.prepare(`SELECT * FROM tasks`).all();
   }
   res.status(200).json({
     message: "tasks are fetched successfully",
@@ -32,9 +45,10 @@ export const getAllTaskController = (req, res) => {
 
 export const getTaskByIdController = (req, res) => {
   const Id = Number(req.params.id);
-  const result = tasks.find((el) => {
-    return el.id === Id;
-  });
+  const result = db.prepare(`
+    SELECT * FROM tasks
+    WHERE id = ?
+    `).get(Id);
   if (!result) {
     return res.status(404).json({
       error: `Task ${Id} not found`,
